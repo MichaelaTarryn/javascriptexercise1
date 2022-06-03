19919cfcb11fa937faf18e47848919ef3369104c
Michaela <Michaela-13>
2022-06-03 14:48:56 +0200

    first

---
+++ code.js
@@ -0,0 +1,61 @@
+// javascript comment
+// console.log(/"My first JavaScript code"/);
+
+
+/*function myFunction() {
+document.getElementById("demo").innerHTML = "Paragraphchanged.";
+}
+*/
+
+
+// function myFunction(){
+//     alert("Hello! My name is (Michaela Ward)!!");
+//     }
+   
+//   //  approach
+//     function display(){
+//         alert('hello world!');
+//     }
+    
+    
+//     const btnDisplay=document.getElementById('btnMadeby')
+
+    // approach2
+    // btnDisplay.addEventListener('click',()=>{
+    //     alert('hello world!');
+    // })
+   // btnDisplay.addEventListener('click',display);
+
+    // SHOW Paragraphy
+    // function call() {
+    //     document.getElementById("demo").innerHTML = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi architecto, doloremque blanditiis ratione id nihil dignissimos cumque ea? Enim, aut.";
+    // }
+    //   hide paragraphy
+    // function call1() {
+    //     document.getElementById("demo").innerHTML = "";
+    // }
+
+    // form example 
+    // let btnshow = document.getElementById('btnshow');
+    // btnshow.addEventListener('click',()=>{
+    //     var fname = document.getElementById('fname').value;
+    //     var surname = document.getElementById('surname').value;
+    //     document.getElementById('shows').innerHTML = fname + " " + surname;
+    // });
+   
+
+
+    // var myStr = 'I am a \"double quoted\" string inside \"double quotes\".'; 
+
+    // console.log(myStr);
+    // display a sentence in comment section
+    // let firstName ='Peter';
+    // let lastName='Henk';
+    // let age =17;
+    // console.log("My name is "+firstName+",\nSurname is"+lastName +",\nAge is "+age+"years old");
+
+    // Another method
+    // let firstName ='Peter';
+    // let lastName='Henk';
+    // let age =17;
+    // console.log("My name is " .concat(firstName,",\nSurname is ",lastName ,",\nAge is ",age," years old"));
